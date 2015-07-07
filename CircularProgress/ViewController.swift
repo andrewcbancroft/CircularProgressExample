4d8d0c671a8cdcb19c7b2a812198fee10a37fa62
Andrew Bancroft <acbancroft@NEWELLMA-MAC.local>
2015-07-07 12:56:06 -0500

    initial commit

--- CircularProgress/ViewController.swift
+++ CircularProgress/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circularProgressView: KDCircularProgress!
+    
+    var currentProgress = 0.0
+    let maxProgress = 5.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,22 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func increaseProgressButton(sender: UIButton) {
+        if currentProgress != maxProgress {
+            currentProgress += 1
+            let newAngleValue = newAngle()
+            
+            circularProgressView.animateToAngle(newAngleValue, duration: 0.5, completion: nil)
+        }
+    }
+    
+    @IBAction func resetButton(sender: UIButton) {
+        currentProgress = 0
+        circularProgressView.animateFromAngle(circularProgressView.angle, toAngle: 0, duration: 0.5, completion: nil)
+    }
+    
+    func newAngle() -> Int {
+        return Int(360 * (currentProgress / maxProgress))
+    }
 }
 
