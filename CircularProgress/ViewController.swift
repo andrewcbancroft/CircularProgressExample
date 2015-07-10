963000d9b0995222945d6d91be74f12bad0a8e82
Andrew Bancroft <andrewcbancroft@gmail.com>
2015-07-09 22:45:38 -0500

    Minor adjustments to code layout. Reset angle to 0 in viewDidLoad

--- CircularProgress/ViewController.swift
+++ CircularProgress/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var circularProgressView: KDCircularProgress!
     
-    var currentProgress = 0.0
-    let maxProgress = 5.0
+    var currentCount = 0.0
+    let maxCount = 5.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+		circularProgressView.angle = 0
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,22 +27,22 @@ class ViewController: UIViewController {
     }
 
 
-    @IBAction func increaseProgressButton(sender: UIButton) {
-        if currentProgress != maxProgress {
-            currentProgress += 1
+    @IBAction func increaseProgressButtonTapped(sender: UIButton) {
+        if currentCount != maxCount {
+            currentCount += 1
             let newAngleValue = newAngle()
             
             circularProgressView.animateToAngle(newAngleValue, duration: 0.5, completion: nil)
         }
     }
-    
-    @IBAction func resetButton(sender: UIButton) {
-        currentProgress = 0
+	
+	func newAngle() -> Int {
+		return Int(360 * (currentCount / maxCount))
+	}
+	
+    @IBAction func resetButtonTapped(sender: UIButton) {
+        currentCount = 0
         circularProgressView.animateFromAngle(circularProgressView.angle, toAngle: 0, duration: 0.5, completion: nil)
-    }
-    
-    func newAngle() -> Int {
-        return Int(360 * (currentProgress / maxProgress))
     }
 }
 
